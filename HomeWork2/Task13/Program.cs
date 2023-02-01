b34cbc2cec688918af21f78880ffd8f9bb426a2b
Vladimir Siyatelev <siyatelev@yandex.ru>
2023-02-01 04:52:35 +0300

    Add HomeWork Lesson 2 Task 10, 13, 15

---
+++ HomeWork2/Task13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите разрядность максимального числа: "); 
+int N = int.Parse(Console.ReadLine());
+int a1 = (int)Math.Pow(10, N);
+int num = new Random().Next(1, a1); //оочень долго ломался на этом.., пока не понял что из double в int нужно выбираться.
+Console.WriteLine(num);
+
+if (num / 100 == 0)
+{
+    Console.WriteLine($"Наше число {num} и третьей цифры в нем нет");
+}
+int index = 3;
+while (index <= N)
+{
+    if (num / ((int)Math.Pow(10, index)) == 0)
+    {
+        int Third = (num / (int)Math.Pow(10, (index-3)))%10;
+        Console.WriteLine($"Наше число {num} и третья цифра в нем {Third}");
+        break;
+    }
+    index++;
+}
---
+++ HomeWork2/Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7) Console.WriteLine("Выходной^^");
+else if (day >= 1 && day <= 5) Console.WriteLine("Рабочий.._(");
+else Console.WriteLine("Нет такого дня!!");
+
