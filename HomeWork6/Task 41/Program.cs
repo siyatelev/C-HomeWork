0a3d604dae2f3a8c1d2faf1bb381eb9a3ea14d41
Vladimir Siyatelev <siyatelev@yandex.ru>
2023-02-15 22:14:04 +0300

    add HomeWork 6

---
+++ HomeWork6/Task 41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите длину массива");
+int size = int.Parse(Console.ReadLine());
+int[] array = Input(size);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Количество положительных чисел: {PositiveNum(array)}");
+
+int[] Input(int length)
+{
+    Console.WriteLine("Заполните массив");
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+    return arr;
+}
+
+int PositiveNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
---
+++ HomeWork6/Task 43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = 0;
+double y = 0;
+
+if (k1 == k2) Console.WriteLine("Прямые не пересекаются");
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+    x = Math.Round(x, 1);
+    y = Math.Round(y, 1);
+    Console.WriteLine($"Прямые пересекаются в точке: ({x};{y})");
+}
